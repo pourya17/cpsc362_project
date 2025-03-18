cd5f2a04f5263e993bc7549de70973f869b5e393
JonTPereyra <jontpereyra@gmail.com>
2025-03-18 10:55:15 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include "calculations.h"
 #include "entries.h"
-
-using namespace std;
+#include "reminders.h"
+#include <string>
 
 double weight = 0;
 double height = 0;
@@ -16,21 +16,21 @@ int main() {
     char gender;
 
     // User Input
-    cout << "Enter your weight (lbs): ";
-    cin >> weight;
-    cout << "Enter your height (feet inches): ";
-    cin >> feet >> inches;
-    cout << "Enter your gender (M/F): ";
-    cin >> gender;
-    cout << "Enter your age: ";
-    cin >> age;
-    cout << "Enter your activity level (1-5): ";
-    cout << "\n1. Sedentary (little to no exercise)";
-    cout << "\n2. Lightly active (light exercise 1-3 days per week)";
-    cout << "\n3. Moderately active (moderate exercise 3-5 days per week)";
-    cout << "\n4. Very active (hard exercise 6-7 days per week)";
-    cout << "\n5. Super active (very intense exercise daily)\n";
-    cin >> activityLevel;
+    std::cout << "Enter your weight (lbs): ";
+    std::cin >> weight;
+    std::cout << "Enter your height (feet inches): ";
+    std::cin >> feet >> inches;
+    std::cout << "Enter your gender (M/F): ";
+    std::cin >> gender;
+    std::cout << "Enter your age: ";
+    std::cin >> age;
+    std::cout << "Enter your activity level (1-5): ";
+    std::cout << "\n1. Sedentary (little to no exercise)";
+    std::cout << "\n2. Lightly active (light exercise 1-3 days per week)";
+    std::cout << "\n3. Moderately active (moderate exercise 3-5 days per week)";
+    std::cout << "\n4. Very active (hard exercise 6-7 days per week)";
+    std::cout << "\n5. Super active (very intense exercise daily)\n";
+    std::cin >> activityLevel;
     weight_kg = weight_to_kg(weight);
     height = height_to_cm(feet, inches);
     // Calculate BMR and Caloric Needs for Weight Maintenance 
@@ -42,13 +42,11 @@ int main() {
     if (calories == -1) {
         cout << "Invalid activity level entered." << endl;
     } else {
-        cout << "Your estimated daily nutritional needs: \n" << "Calories:" << calories << " kcal" << endl;
-        cout << "Protein: " << protein << "g" << endl;
-        cout << "Carbs: " << carbs << "g" << endl;
-        cout << "Fats: " << fats << "g" << endl;
-    }     
-
-    Entry entry{"Cereal", 200};
+       std::cout << "Your estimated daily nutritional needs: \n" << "Calories:" << calories << " kcal" << std::endl;
+       std::cout << "Protein: " << protein << "g" << std::endl;
+       std::cout << "Carbs: " << carbs << "g" << std::endl;
+       std::cout << "Fats: " << fats << "g" << std::endl;
+    }
 
    // cout << height << weight_kg;
     return 0;
